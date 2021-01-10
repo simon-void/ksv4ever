a1180ab60a2246b2b7e692b92a42ef38030897f8
Stephan Schroeder <simon.void@gmx.de>
2021-01-10 19:10:27 +0100

    added publishing and bintray config. also new Kotlin format applies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 plugins {
-    kotlin("jvm") version "1.4.20"
-    id("maven-publish")
+    kotlin("jvm") version "1.4.21"
     id("java-library")
+    id("maven-publish")
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "net.simonvoid"
-version = "1.1.2"
+version = "1.1.3"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = java.sourceCompatibility
 
@@ -36,21 +39,109 @@ tasks {
     withType<Test> {
         useTestNG()
     }
+}
 
-    val sourcesJar by creating(Jar::class) {
-        archiveClassifier.set("sources")
-        from(sourceSets.main.get().allSource)
+val artifactName = rootProject.name
+val artifactGroup = project.group.toString()
+val artifactVersion = project.version.toString()
+
+val pomDeveloperId = "simon-void"
+val pomDeveloperName = "Stephan Schroeder"
+
+val githubRepo = "$pomDeveloperId/$artifactName"
+val githubReadme = "README.md"
+
+val pomUrl = "https://github.com/$githubRepo"
+val pomScmUrl = pomUrl
+val pomIssueUrl = "$pomUrl/issues"
+val pomDesc = pomUrl
+
+val pomLicenseName = "MIT"
+val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
+val pomLicenseDist = "repo"
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    dependsOn.add(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>(artifactName) {
+            groupId = artifactGroup
+            artifactId = artifactName
+            version = artifactVersion
+            from(components["java"])
+
+            artifacts {
+                archives(sourcesJar)
+                archives(javadocJar)
+                archives(tasks.jar)
+            }
+
+            pom.withXml {
+                asNode().apply {
+                    appendNode("description", pomDesc)
+                    appendNode("name", artifactName)
+                    appendNode("url", pomUrl)
+                    appendNode("licenses").appendNode("license").apply {
+                        appendNode("name", pomLicenseName)
+                        appendNode("url", pomLicenseUrl)
+                        appendNode("distribution", pomLicenseDist)
+                    }
+                    appendNode("developers").appendNode("developer").apply {
+                        appendNode("id", pomDeveloperId)
+                        appendNode("name", pomDeveloperName)
+                    }
+                    appendNode("scm").apply {
+                        appendNode("url", pomScmUrl)
+                    }
+                }
+            }
+        }
     }
+}
 
-    val javadocJar by creating(Jar::class) {
-        dependsOn.add(javadoc)
-        archiveClassifier.set("javadoc")
-        from(javadoc)
-    }
+// upload to Bintray via
+// ./gradlew bintrayUpload -PbintrayUser="my_bintray_username" -PbintrayKey="my_bintray_api_key"
 
-    artifacts {
-        archives(sourcesJar)
-        archives(javadocJar)
-        archives(jar)
+bintray {
+    user = project.findProperty("bintrayUser").toString()
+    key = project.findProperty("bintrayKey").toString()
+    publish = true
+
+    val repoName = "maven"
+
+    setPublications(artifactName)
+
+    pkg.apply {
+        repo = repoName
+        name = artifactName
+        userOrg = user.toLowerCase()
+        githubRepo = githubRepo
+        vcsUrl = pomScmUrl
+        description = "robust mapping of csv data to user-defined Kotlin data classes"
+        setLabels("kotlin", "csv")
+        setLicenses("MIT")
+        desc = description
+        websiteUrl = pomUrl
+        issueTrackerUrl = pomIssueUrl
+        githubReleaseNotesFile = githubReadme
+
+        version.apply {
+            name = artifactVersion
+            desc = pomDesc
+            released = LocalDateTime.now().let { now->
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                now.format(formatter)
+            }
+            vcsTag = artifactVersion
+        }
     }
 }
